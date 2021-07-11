6d6f4b23fbb65fcd853db181ebc716223381adec
bibekdhkl <bibekdh2016@gmail.com>
2021-07-11 16:35:21 +0545

    :tada:Features added and Modified

---
+++ app.js
@@ -0,0 +1,12 @@
+function addText(){
+      document.getElementById("text_area").innerText= "☕️☕️☕️";
+}
+function removeText(){
+      document.getElementById("text_area").innerText= "";
+}
+
+//Scroll to top button
+
+function scrollToTop() {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+}
